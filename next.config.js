0b6448f701709bae3bb77c430b7a283c2d0f1086
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2023-09-21 11:40:59 +0530

    delete env file add to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
 
     output: 'standalone',
+    env:{
+        GITHUB_ID: process.env.GITHUB_ID,
+        GITHUB_SECRET: process.env.GITHUB_SECRET,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        BASE_URL: process.env.BASE_URL,
+    }
 }
 
 module.exports = nextConfig

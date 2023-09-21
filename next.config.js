a35686b243314fa71b228eadc8369b55fd190b5e
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2023-09-21 09:25:52 +0530

    Adjust next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        GITHUB_ID: process.env.GITHUB_ID,
-        GITHUB_SECRET: process.env.GITHUB_SECRET,
-    },
+
     // output: 'standalone',
 }
 
